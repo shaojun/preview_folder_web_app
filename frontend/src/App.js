b02dbb684586c13c4d0fda03b65db4ec2b2136b0
shaothinkbookubuntu <157350686@qq.com>
2024-09-24 11:48:51 +0800

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,11 @@
-import React, { useState } from 'react';
-import Sidebar from './components/Sidebar';
+import React from 'react';
 import FileExplorer from './components/FileExplorer';
-import FilePreview from './components/FilePreview';
 import './App.css';
 
 function App() {
-  const [selectedPath, setSelectedPath] = useState('');
-  const [selectedFile, setSelectedFile] = useState(null);
-
   return (
     <div className="app">
-      <Sidebar />
-      <div className="main-content">
-        <FileExplorer
-          selectedPath={selectedPath}
-          setSelectedPath={setSelectedPath}
-          setSelectedFile={setSelectedFile}
-        />
-        <FilePreview selectedFile={selectedFile} />
-      </div>
+      <FileExplorer />
     </div>
   );
 }
